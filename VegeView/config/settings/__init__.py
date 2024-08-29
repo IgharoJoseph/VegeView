67cceaa77812424a5fc0bc4b3beeed98aeacd29e
IgharoJoseph <josephigharo@gmail.com>
2024-08-29 01:09:53 +0100

    New edit again

--- VegeView/config/settings/__init__.py
+++ VegeView/config/settings/__init__.py
@@ -1,9 +1,8 @@
 import os
 from pathlib import Path
 from split_settings.tools import include
+from decouple import config
 
 include(
-    "base.py",
-    "local.py",
-    "prod.py",
+    'base.py'
 )
